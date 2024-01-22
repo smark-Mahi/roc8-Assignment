ac04f004e40ea88f81311f1ab9726b5baf15b6b4
kmzee-sm <133731546+kmzee-sm@users.noreply.github.com>
2024-01-22 08:49:32 +0530

    changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,25 @@
 "use client";
-import {  useState } from "react";
+import { useEffect, useState } from "react";
 import { IoIosHeartEmpty } from "react-icons/io";
 import Link from "next/link";
 import { useGlobalStates } from "@/store/globalState";
 import { FaHeart } from "react-icons/fa6";
-// import { useSession, signOut } from "next-auth/react";
-// import { useRouter } from "next/navigation";
+import { useSession, signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
 const Navbar = () => {
   const [openMenu, setOpenMenu] = useState<boolean>(false);
   const { favourites } = useGlobalStates();
-  // const { status } = useSession();
-  // const router = useRouter();
-  // function logoutHandler() {
-  //   signOut();
-  // }
+  const { status } = useSession();
+  const router = useRouter();
+  function logoutHandler() {
+    signOut();
+  }
 
-  // useEffect(() => {
-  //   if (status === "unauthenticated") {
-  //     router.push("/login");
-  //   }
-  // }, [status]);
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      router.push("/login");
+    }
+  }, [status]);
 
   return (
     <div className="flex flex-col items-center h-fit py-2 px-4 bg-[rgba(82,82,112,0.9)] border-solid border-2 border-[#a09f9f] rounded-md md:m-8 m-4">
@@ -35,28 +35,27 @@ const Navbar = () => {
           >
             History
           </Link>
-          {/* {status === "authenticated" && (
+          {status === "authenticated" && (
             <p
               className="nav-text duration-300 ease-linear transition-all hover:text-opacity-30 hover:underline"
               onClick={logoutHandler}
             >
               Logout
             </p>
-          )} */}
-          {/* {status === "unauthenticated" && ( */}
+          )}
+          {status === "unauthenticated" && (
             <Link
               href="/login"
               className="nav-text duration-300 ease-linear transition-all hover:text-opacity-30 hover:underline"
-              // onClick={logoutHandler}
             >
               Login
             </Link>
-          {/* )} */}
-          {/* {status !== "authenticated" && ( */}
+          )}
+          {status !== "authenticated" && (
             <Link href="/login">
               <button className="btn-style">Create Account</button>
             </Link>
-          {/* )} */}
+          )}
           <Link href="/favourites">
             {favourites.length === 0 ? (
               <IoIosHeartEmpty className="text-2xl" role="button" />
@@ -106,22 +105,22 @@ const Navbar = () => {
           >
             Favourite
           </Link>
-          {/* {status === "authenticated" && (
+          {status === "authenticated" && (
             <p
               className="nav-text w-full  py-6 hover:opacity-60 "
               onClick={logoutHandler}
             >
               Logout
             </p>
-          )} */}
-          {/* {status === "unauthenticated" && ( */}
+          )}
+          {status === "unauthenticated" && (
             <Link
               href="/login"
               className="nav-text w-full  py-6 hover:opacity-60 "
             >
               Login
             </Link>
-          {/* )} */}
+          )}
         </nav>
       </section>
     </div>
