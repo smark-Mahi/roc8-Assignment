b9aac6a2d98b4f40e1dfc2793a68398c5e500cef
kmzee-sm <133731546+kmzee-sm@users.noreply.github.com>
2024-01-22 08:40:01 +0530

    changes

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -20,12 +20,11 @@ const Login = () => {
 
   return (
     <div className="h-screen flex justify-center items-center ">
-      <div className="flex flex-col justify-around h-48 gap-2 px-4 rounded-sm border-solid border-[1.5px] border-gray-300  md:min-w-96 w-80">
+      <div className="flex flex-col justify-around h-48 gap-2 px-4 rounded-sm ring-1  md:min-w-96 w-80">
         <h1 className="font-bold text-2xl self-center">Sign Up or Log In</h1>
         <button
           key="github"
           className="bg-slate-800 h-10 rounded-md ring-2 "
-
           onClick={handleLogin}
         >
           <p className="flex justify-around items-center text-white">
